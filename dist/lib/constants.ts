9d8544c02e4d47af0086bc790ad3594bc39a7c84
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2025-02-06 22:54:41 +0700

    secord commit

--- dist/lib/constants.ts
+++ dist/lib/constants.ts
@@ -5,8 +5,8 @@ import { Github, Facebook, Youtube, MessageCircle, Mail } from 'lucide-react'
 export const socialLinks = [
   { icon: Github, name: "GitHub", href: "https://github.com/konnn04", color: "#333" },
   { icon: Facebook, name: "Facebook", href: "https://facebook.com/konnn.1011", color: "#1877f2" },
-  { icon: Youtube, name: "Youtube", href: "https://youtube.com/konnn04", color: "#ff0000" },
-  { icon: MessageCircle, name: "Discord", href: "https://discord.gg/konnn_04", color: "#7289da" },
+  { icon: Youtube, name: "Youtube", href: "https://www.youtube.com/@Konnn", color: "#ff0000" },
+  { icon: MessageCircle, name: "Discord: konnn_04", href: "https://discord.gg/", color: "#7289da" },
   { icon: Mail, name: "Email", href: "mailto:'trieukon1011@gmail.com'", color: "#333" },
 ]
 
