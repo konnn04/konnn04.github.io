5d2818ef2effbe2b9285959c98075833065cc861
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2025-04-06 16:16:30 +0700

    build static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'; // Change adapter
+import adapter from '@sveltejs/adapter-static'; // Change adapter
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
